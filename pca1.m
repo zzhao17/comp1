c0fb33f511bf8aef076c615ed0e3614ad49f025c
ShuyingTang <st765@cornell.edu>
2016-10-28 01:38:21 -0400

    Update pca1.m

--- pca1.m
+++ pca1.m
@@ -1,14 +1,14 @@
-function [afterpca] = pca1()
-% PCA1: Perform PCA
-% data - MxN matrix of input data 
-% (M dimensions, N trials) 
-data = csvread('features.csv');
-dim = 10;
+function [afterpca] = pca(data,k)
+% Shuying Tang, cs4786, Competion 1
+% Perform PCA, we normalize data in this function, free to comment out
+% normalization
+% data - MxN matrix of input data  
+% k = dimensions we want to reduce to 
 [M,N] = size(data); 
 % subtract off the mean for each dimension 
-mn = mean(data,2); 
-data = data - repmat(mn,1,N);
-% normalize
+mn = mean(data,1); 
+data = data - repmat(mn,M,1);
+% normalize input data
 var = 1/(M-1) * ( sum((data).^2));
 for i = 1:1:N
     data (:,i) = sqrt( 1/ (var(:,i)) ) * data(:,i);
@@ -18,13 +18,13 @@ covariance = cov(data);
 % find the eigenvectors and eigenvalues 
 [envec, enval] = eig(covariance);
 v = diag(enval);
-% sort the variances in decreasing order 
+% sort the diag eigenvalues in decreasing order 
 [junk, rindices] = sort(-1*v);
-transform = [];
-for i=1:dim
-    row_num = rindices(i,:);
-    add = envec(:,row_num);
-    transform = [transform add];
+transform = zeros(N,k);
+for i=1:k
+    col_num = rindices(i,:);
+    add = envec(:,col_num);
+    transform(:,i) = add;
 % project the original data set 
 afterpca = data * transform;
 end
